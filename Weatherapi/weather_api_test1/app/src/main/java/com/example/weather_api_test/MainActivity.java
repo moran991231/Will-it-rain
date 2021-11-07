31f1d9d9226d60ea54492c80724d4e0a2130b6dc
moran991231 <moran991231@naver.com>
2021-11-07 22:57:06 +0900

    auto basetime+api+noti + authority

--- Weatherapi/weather_api_test1/app/src/main/java/com/example/weather_api_test/MainActivity.java
+++ Weatherapi/weather_api_test1/app/src/main/java/com/example/weather_api_test/MainActivity.java
@@ -1,28 +1,61 @@
 package com.example.weather_api_test;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import org.json.JSONException;
-
-import java.io.IOException;
-import java.util.concurrent.Executors;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        OnCheckPermission();
         // the usage of api
-        apiTest at = new apiTest();
-        int ret = at.isGoingToRain("20211107", "1700", 24, 61, 127);
+        Weather at = new Weather();
+        int ret = at.isGoingToRain(24, 61, 127);
         System.out.println("@@@@@@@ ret: " + ret);
 
         MyNotification myNoti = new MyNotification(this, MyNotification.DefaultNotiID);
         String text = at.makeNotificatoinText(10, ret);
-        myNoti.makeNotification("hey", text);
+        myNoti.makeNotification("Will it rain?", text);
 
+    }
+
+    static final int PERMISSIONS_REQUEST = 0x0000001;
+
+    public void OnCheckPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Toast.makeText(this, "앱 실행을 위해서는 권한을 설정해야 합니다", Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSIONS_REQUEST);
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSIONS_REQUEST);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grandResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grandResults);
+
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST:
+                if (grandResults.length > 0 && grandResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "앱 실행을 위한 권한이 설정 되었습니다", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "앱 실행을 위한 권한이 취소 되었습니다", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 }
